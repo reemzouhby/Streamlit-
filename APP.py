ad7b7c7f4855570841eac93aab28f8fe5f5c04e8
reemzouhby <111857490+reemzouhby@users.noreply.github.com>
2025-08-14 22:19:07 +0300

    Update APP.py

--- APP.py
+++ APP.py
@@ -69,7 +69,7 @@ def create_model():
 
         # Train model
         model.fit(train_images, train_labels,
-                  epochs=3,
+                  epochs=10,
                   batch_size=128,
                   validation_data=(test_images, test_labels),
                   verbose=0)
